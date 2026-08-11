72ecf6d81eb48ca92479c081894efad387de74b4
lefv <luis@lefv.io>
2026-08-11 09:33:42 +0200

    fix: nix shell evaluates on darwin, and prompt paths accept ~
    The dev shell and package pulled glibc.dev for BINDGEN_EXTRA_CLANG_ARGS,
    inherited from the eilmeldung fork. glibc is Linux-only in nixpkgs, so
    `direnv allow` failed to evaluate on aarch64-darwin. No crate in the
    lockfile links C, so the whole bindgen/cmake/openssl/sqlite/libxml2 block
    is dead weight and comes out with it.
    
    Config::load only expanded ~ in workspace.root, so a prompt path shared
    between machines resolved on one and silently fell back to the built-in
    prompt on the other.

--- nix/package.nix
+++ nix/package.nix
@@ -1,53 +1,19 @@
 { lib
 , rustPlatform
-, pkg-config
-, cmake
-, perl
-, openssl
-, libxml2
-, sqlite
-, glib
-, glibc
-, llvmPackages_19
 }:
 
 rustPlatform.buildRustPackage {
   pname = "mitodo";
-  version = "1.1.0";
-  
+  version = (lib.importTOML ../Cargo.toml).package.version;
+
   src = ../.;
-  
+
   cargoLock = {
     lockFile = ../Cargo.lock;
   };
-  
-  nativeBuildInputs = [
-    pkg-config
-    cmake
-    perl  
-  ];
-  
-  buildInputs = [
-    openssl
-    libxml2
-    sqlite
-  ];
-  
-  LIBCLANG_PATH = lib.makeLibraryPath [ llvmPackages_19.libclang.lib ];
-  BINDGEN_EXTRA_CLANG_ARGS = lib.concatStringsSep " " (
-    (builtins.map (a: ''-I"${a}/include"'') [
-      glibc.dev
-    ])
-    ++ [
-      ''-I"${llvmPackages_19.libclang.lib}/lib/clang/19/include"''
-      ''-I"${glib.dev}/include/glib-2.0"''
-      ''-I${glib.out}/lib/glib-2.0/include/''
-      ''-I"${glibc.dev}/include/"''
-    ]
-  );
-  
+
   meta = with lib; {
-    description = "A feature-rich TUI RSS Reader based on the news-flash library";
+    description = "a TUI todo tracker over plain markdown checklists";
     homepage = "https://github.com/gwicho38/mitodo";
     license = licenses.gpl3Plus;
     maintainers = [ "gwicho38" ];
--- nix/shell.nix
+++ nix/shell.nix
@@ -1,45 +1,11 @@
 { pkgs }:
 
 pkgs.mkShell {
-  # Development tools
   nativeBuildInputs = with pkgs; [
-    # Rust toolchain
     cargo
     rustc
     rust-analyzer
     clippy
     rustfmt
-    
-    # Build tools
-    pkg-config
-    cmake
-    
-    # Debugging and profiling
-    llvmPackages_19.libclang
-    llvmPackages_19.clang
   ];
-
-  # Libraries needed for building
-  buildInputs = with pkgs; [
-    openssl
-    libxml2
-    sqlite
-    glibc
-    glibc.dev
-  ];
-
-  # Environment variables for development
-  LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_19.libclang.lib ];
-  BINDGEN_EXTRA_CLANG_ARGS = builtins.concatStringsSep " " (
-    (map (a: ''-I"${a}/include"'') [
-      pkgs.glibc.dev
-    ])
-    ++ [
-      ''-I"${pkgs.llvmPackages_19.libclang.lib}/lib/clang/19/include"''
-      ''-I"${pkgs.glib.dev}/include/glib-2.0"''
-      ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
-      ''-I"${pkgs.glibc.dev}/include/"''
-    ]
-  );
-  
 }
